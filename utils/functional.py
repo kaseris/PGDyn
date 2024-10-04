9f7429bb9e30c03692d35041d59981dfe82a9f88
Michail Kaseris <mich.kaseris@gmail.com>
2024-10-04 14:04:58 +0300

    Read configuration yaml files.

--- utils/functional.py
+++ utils/functional.py
@@ -1,3 +1,11 @@
-def gen_velocity(m):
+import yaml
+import torch
+
+def gen_velocity(m: torch.Tensor):
     dm = m[:, 1:] - m[:, :-1]
     return dm
+
+def read_config(fname: str):
+    with open(fname) as f:
+        config = yaml.safe_load(f)
+    return config
